171baab7f4c83e0119ae5c242e9cec2a9236368b
Amwighio <159983597+manan7866@users.noreply.github.com>
2024-02-27 22:10:21 -0800

    Add files via upload

---
+++ class assigment.js
@@ -0,0 +1,23 @@
+"use strict";
+// Data types
+// string
+let name = "Abdul Manan";
+console.log(name);
+// Number
+let age = 20;
+console.log(age);
+// boolean
+let instudy = true;
+console.log(instudy);
+// null
+let Dreams = null;
+console.log(Dreams);
+// undifined
+let cast;
+console.log(cast);
+// biginit
+let amount = (123456789098765);
+console.log(amount);
+//symbol
+let mycity = "karachi";
+console.log(mycity);
---
+++ class assigment.ts
@@ -0,0 +1,32 @@
+// Data types
+
+// string
+let name = "Abdul Manan"
+console.log(name)
+
+// Number
+let age = 20;
+console.log(age)
+
+// boolean
+
+let instudy = true;
+console.log(instudy)
+
+// null
+let Dreams = null;
+console.log(Dreams)
+
+// undifined
+
+let cast
+console.log(cast)
+
+// biginit
+
+let amount : bigint = (123456789098765);
+console.log(amount)
+
+//symbol
+let mycity : symbol = "karachi"
+console.log(mycity)
